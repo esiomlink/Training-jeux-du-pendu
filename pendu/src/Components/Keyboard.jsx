0abe990c5560f67b45ea61027595941534e9754b
ecoffet <simon.ecoffet@gmail.com>
2021-04-11 16:06:06 +0200

    correctif pendu hooks settings

--- pendu/src/Components/Keyboard.jsx
+++ pendu/src/Components/Keyboard.jsx
@@ -1,36 +1,51 @@
 import React from 'react';
 
-
 const Keyboard = () => {
-  const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  const [used,setUsed] = React.useState("")
+  const [alphabet, setAlphabet] = React.useState([
+    { letter: 'a', used: 0 },
+    { letter: 'b', used: 0 },
+    { letter: 'c', used: 0 },
+    { letter: 'd', used: 0 },
+    { letter: 'e', used: 0 },
+    { letter: 'f', used: 0 },
+    { letter: 'g', used: 0 },
+    { letter: 'h', used: 0 },
+    { letter: 'i', used: 0 },
+    { letter: 'j', used: 0 },
+    { letter: 'k', used: 0 },
+    { letter: 'l', used: 0 },
+    { letter: 'm', used: 0 },
+    { letter: 'n', used: 0 },
+    { letter: 'o', used: 0 },
+    { letter: 'p', used: 0 },
+    { letter: 'q', used: 0 },
+    { letter: 'r', used: 0 },
+    { letter: 's', used: 0 },
+    { letter: 't', used: 0 },
+    { letter: 'u', used: 0 },
+    { letter: 'v', used: 0 },
+    { letter: 'w', used: 0 },
+    { letter: 'x', used: 0 },
+    { letter: 'y', used: 0 },
+    { letter: 'z', used: 0 },
+  ]);
+  console.log(alphabet);
   
-  console.log(used)
-
-const restant = alphabet.filter(letter=> letter!== used)
-  console.log(restant)
-  
-
-
-
-
-
- 
   return (
-    <div>{
-   
-      restant.map(
-        (letter, key)=>{
-          return <button 
-          onClick={()=>setUsed(used+letter)  }
-          >{letter}</button>
-        }
-        )
-        
-      }
+    <div>
+      {alphabet
+      .filter((used)=>used.used!==1)
+      .map((letters) => {
+        return (
+           <button 
+           onClick={() => setAlphabet([...alphabet,{...letters.used=1}])}
+           >
+            {letters.letter}
+          </button>
+        );
+      })}
     </div>
   );
-
-}
+};
 
 export default Keyboard;
