b5f09f87b866d3fcdffcff1ca64574ba6317ba43
ecoffet <simon.ecoffet@gmail.com>
2021-04-11 23:47:26 +0200

    up state variable for good dispatch children state props

--- pendu/src/Components/Keyboard.jsx
+++ pendu/src/Components/Keyboard.jsx
@@ -1,44 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const Keyboard = () => {
-  const [alphabet, setAlphabet] = React.useState([
-    { letter: 'a', used: 0 },
-    { letter: 'b', used: 0 },
-    { letter: 'c', used: 0 },
-    { letter: 'd', used: 0 },
-    { letter: 'e', used: 0 },
-    { letter: 'f', used: 0 },
-    { letter: 'g', used: 0 },
-    { letter: 'h', used: 0 },
-    { letter: 'i', used: 0 },
-    { letter: 'j', used: 0 },
-    { letter: 'k', used: 0 },
-    { letter: 'l', used: 0 },
-    { letter: 'm', used: 0 },
-    { letter: 'n', used: 0 },
-    { letter: 'o', used: 0 },
-    { letter: 'p', used: 0 },
-    { letter: 'q', used: 0 },
-    { letter: 'r', used: 0 },
-    { letter: 's', used: 0 },
-    { letter: 't', used: 0 },
-    { letter: 'u', used: 0 },
-    { letter: 'v', used: 0 },
-    { letter: 'w', used: 0 },
-    { letter: 'x', used: 0 },
-    { letter: 'y', used: 0 },
-    { letter: 'z', used: 0 },
-  ]);
-  console.log(alphabet);
+
+const Keyboard = (props) => {
+  const {alphabet , setAlphabet} = props
+  console.log(alphabet , );
+  
   
   return (
     <div>
       {alphabet
-      .filter((used)=>used.used!==1)
-      .map((letters) => {
+      .map((letters, key) => {
         return (
            <button 
-           onClick={() => setAlphabet([...alphabet,{...letters.used=1}])}
+           onClick={() => setAlphabet([...alphabet,{...letters.letter="#"}])}
            >
             {letters.letter}
           </button>
