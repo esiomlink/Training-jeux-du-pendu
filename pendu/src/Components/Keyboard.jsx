b9a591b0a4401338c89855a590bee9ffe113388b
ecoffet <simon.ecoffet@gmail.com>
2021-04-11 09:55:04 +0200

    add filter for filter letter

--- pendu/src/Components/Keyboard.jsx
+++ pendu/src/Components/Keyboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import App from '../App';
+
 
 const Keyboard = () => {
   const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   const [used,setUsed] = React.useState("")
-
-
+  
   console.log(used)
+
+const restant = alphabet.filter(letter=> letter!== used)
+  console.log(restant)
   
 
 
@@ -16,10 +18,11 @@ const Keyboard = () => {
  
   return (
     <div>{
-      alphabet.map(
+   
+      restant.map(
         (letter, key)=>{
           return <button 
-          onClick={()=>setUsed(used+letter)}
+          onClick={()=>setUsed(used+letter)  }
           >{letter}</button>
         }
         )
